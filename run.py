9943b40ab6f26e2aab2017a71ee775a1ddbe6b02
hfy001 <49182366+hfy001@users.noreply.github.com>
2020-12-09 10:54:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ import os
 if __name__ == '__main__':
     pytest.main(["-s","-q",
                  "--alluredir", "./allure-results"])
-    os.system(r"allure generate --clean allure-results -o allure-report")
+#     os.system(r"allure generate --clean allure-results -o allure-report")
     # pytest.main(["test_case/test_log.py"])
     # pytest.main(['-s', '-q', '--alluredir', './report/xml'])
